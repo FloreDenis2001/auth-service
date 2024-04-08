2f407aad524ea7283a6ff494f0b2ce295d5db1df
FloreDenis2001 <floredenis907@yahoo.com>
2024-04-08 13:32:29 +0300

    lasT_update

--- src/main/java/org/authservice/AuthApplication.java
+++ src/main/java/org/authservice/AuthApplication.java
@@ -30,7 +30,7 @@ public class AuthApplication {
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200", "http://localhost"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200", "http://localhost" , "http://localhost:5000"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Allow-Headers"));
